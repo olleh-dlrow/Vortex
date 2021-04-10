4f8324ab94f62be2ed1215c7a7536245c23c72cb
olleh-dlrow <1136246408@qq.com>
2021-04-10 19:16:24 +0800

    add premake

--- Vortex/src/Vortex/Application.cpp
+++ Vortex/src/Vortex/Application.cpp
@@ -4,6 +4,8 @@
 #include "Events/ApplicationEvent.h"
 #include "Log.h"
 
+#include "GLFW/glfw3.h"
+
 namespace Vortex {
 
     void Application::Run() {
@@ -11,6 +13,18 @@ namespace Vortex {
         VT_TRACE(e);
         VT_INFO("hello, now I can load Vortex.dll automatically!!");
         //std::cout << e << std::endl;
-        while(true);
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        GLFWwindow *window = glfwCreateWindow(800, 600, "Vortex", NULL, NULL);
+        glfwMakeContextCurrent(window);
+        while(!glfwWindowShouldClose(window)) {
+            glClearColor(0.1f, 0.3f, 0.1f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+        //while(true);
     }
 }
---
+++ premake5.lua
@@ -0,0 +1,127 @@
+workspace "Vortex"
+    architecture "x64"
+    startproject "Sandbox"
+    
+    -- Dist: distination
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Vortex/vendor/GLFW/include"
+
+include "Vortex/vendor/GLFW"
+
+project "Vortex"
+    location "Vortex"
+    kind "SharedLib"
+    language "C++"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    pchheader "vtpch.h"
+    pchsource "Vortex/src/vtpch.cpp"
+    
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+    
+    includedirs
+    {
+        "%{prj.name}/src",
+        "%{prj.name}/vendor/spdlog/include",
+        "%{IncludeDir.GLFW}"
+    }
+    
+    links
+    {
+        "GLFW",
+        "opengl32.lib"
+    }
+    
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+        
+        defines
+        {
+            "VT_PLATFORM_WINDOWS",
+            "VT_BUILD_DLL"
+        }
+    
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+        
+    filter "configurations:Debug"
+        defines "VT_DEBUG"
+        symbols "On"
+        
+    filter "configurations:Release"
+        defines "VT_RELEASE"
+        symbols "On"    
+        
+    filter "configurations:Dist"
+        defines "VT_DIST"
+        symbols "On"       
+
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+    
+    includedirs
+    {
+        "Vortex/vendor/spdlog/include",
+        "Vortex/src"
+    }
+    
+    links
+    {
+        "Vortex"
+    }
+    
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+        
+        defines
+        {
+            "VT_PLATFORM_WINDOWS"
+        }
+        
+    filter "configurations:Debug"
+        defines "VT_DEBUG"
+        symbols "On"
+        
+    filter "configurations:Release"
+        defines "VT_RELEASE"
+        symbols "On"    
+        
+    filter "configurations:Dist"
+        defines "VT_DIST"
+        symbols "On"           
+ 
