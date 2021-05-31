e6f9e37fee4ce4ca34b8edeeb2066bea640452a7
olleh-dlrow <1136246408@qq.com>
2021-05-31 11:27:30 +0800

    add openglshader and uniform function

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include <Vortex.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 /*
 tips:
@@ -94,9 +97,9 @@ public:
 			}
 		)";
 
-        m_Shader.reset(new Vortex::Shader(vertexSrc, fragmentSrc));
+        m_Shader.reset(Vortex::Shader::Create(vertexSrc, fragmentSrc));
 
-        std::string blueShaderVertexSrc = R"(
+        std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -112,19 +115,21 @@ public:
 			}
 		)";
 
-        std::string blueShaderFragmentSrc = R"(
+        std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+
+            uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-        m_BlueShader.reset(new Vortex::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
-
+        m_FlatColorShader.reset(Vortex::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
     }
 
     void OnUpdate(Vortex::Timestep ts) override
@@ -155,6 +160,10 @@ public:
         Vortex::Renderer::BeginScene(m_Camera);
 
         glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+        
+        // question: we still need to convert Shader to OpenGLShder, could this make packing shader no meaning?
+        std::dynamic_pointer_cast<Vortex::OpenGLShader>(m_FlatColorShader)->Bind();
+        std::dynamic_pointer_cast<Vortex::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
 
         for (int y = 0; y < 20; y++)
         {
@@ -162,7 +171,7 @@ public:
             {
                 glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
                 glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-                Vortex::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+                Vortex::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
             }
         }
 
@@ -173,7 +182,9 @@ public:
 
     virtual void OnImGuiRender() override
     {
-
+        ImGui::Begin("Settings");
+        ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+        ImGui::End();
     }
 
     void OnEvent(Vortex::Event& event) override
@@ -185,7 +196,7 @@ private:
     std::shared_ptr<Vortex::Shader> m_Shader;
     std::shared_ptr<Vortex::VertexArray> m_VertexArray;
 
-    std::shared_ptr<Vortex::Shader> m_BlueShader;
+    std::shared_ptr<Vortex::Shader> m_FlatColorShader;
     std::shared_ptr<Vortex::VertexArray> m_SquareVA;
 
     Vortex::OrthographicCamera m_Camera;
@@ -194,6 +205,8 @@ private:
 
     float m_CameraRotation = 0.0f;
     float m_CameraRotationSpeed = 180.0f;
+
+    glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox: public Vortex::Application {
