72a452e75094724f2820d56efac7abbd06071d3b
1136246408@qq.com <1136246408@qq.com>
2021-08-04 13:05:26 +0800

    add timer to sandbox2d

--- Vortex/src/Platform/OpenGL/OpenGLShader.cpp
+++ Vortex/src/Platform/OpenGL/OpenGLShader.cpp
@@ -57,10 +57,18 @@ namespace Vortex
         if (in)
         {
             in.seekg(0, std::ios::end);
-            result.resize(in.tellg());
-            in.seekg(9, std::ios::beg);
-            in.read(&result[0], result.size());
-            in.close();
+            size_t size = in.tellg();
+            if (size != -1)
+            {
+                result.resize(size);
+                in.seekg(0, std::ios::beg);
+                in.read(&result[0], size);
+                in.close();
+            }
+            else
+            {
+                VT_CORE_ERROR("Could not read from file '{0}'", filepath);
+            }
         }
         else
         {
--- Vortex/src/Vortex/ImGui/ImGuiLayer.cpp
+++ Vortex/src/Vortex/ImGui/ImGuiLayer.cpp
@@ -88,10 +88,4 @@ namespace Vortex {
             glfwMakeContextCurrent(backup_current_context);
         }
     }
-
-    void ImGuiLayer::OnImGuiRender()
-    {
-        static bool show = true;
-        ImGui::ShowDemoWindow(&show);
-    }
 }
