77deeeb41488efd4fde5f187ccc91884bde93f25
1136246408@qq.com <1136246408@qq.com>
2021-08-05 21:35:10 +0800

    Log now can write to Vortex.log

--- Vortex/src/Vortex/Core/Log.cpp
+++ Vortex/src/Vortex/Core/Log.cpp
@@ -7,12 +7,21 @@ namespace Vortex {
 
 
     void Log::Init() {
-        spdlog::set_pattern("%^[%T] %n: %v%$");
-        s_CoreLogger = spdlog::stdout_color_mt("VORTEX");
-        s_CoreLogger->set_level(spdlog::level::trace);
+        std::vector<spdlog::sink_ptr> logSinks;
+        logSinks.emplace_back(CreateRef<spdlog::sinks::stdout_color_sink_mt>());
+        logSinks.emplace_back(CreateRef<spdlog::sinks::basic_file_sink_mt>("Vortex.log", true));
 
-        s_ClientLogger = spdlog::stdout_color_mt("APP");
+        logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+        logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+        s_CoreLogger = CreateRef<spdlog::logger>("VORTEX", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_CoreLogger);
+        s_CoreLogger->set_level(spdlog::level::trace);
+        s_CoreLogger->flush_on(spdlog::level::trace);
+
+        s_ClientLogger = CreateRef<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_ClientLogger);
         s_ClientLogger->set_level(spdlog::level::trace);
-        VT_CORE_INFO("Init Log: core-trace client-trace");
+        s_ClientLogger->flush_on(spdlog::level::trace);
     }
 }
