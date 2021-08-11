8876b1229ef1d39176e899939fde87413e1e5a24
1136246408@qq.com <1136246408@qq.com>
2021-08-12 00:17:47 +0800

    added draw call splitting and stats

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -34,6 +34,7 @@ void Sandbox2D::OnUpdate(Vortex::Timestep ts)
     }
 
     // Render
+    Vortex::Renderer2D::ResetStats();
     {
         VT_PROFILE_SCOPE("Renderer Prep");
         Vortex::RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
@@ -49,8 +50,20 @@ void Sandbox2D::OnUpdate(Vortex::Timestep ts)
         Vortex::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
         Vortex::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
         Vortex::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-        Vortex::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+        Vortex::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
         Vortex::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
+        Vortex::Renderer2D::EndScene();
+
+
+        Vortex::Renderer2D::BeginScene(m_CameraController.GetCamera());
+        for (float y = -5.0f; y < 5.0f; y += 0.5f)
+        {
+            for (float x = -5.0f; x < 5.0f; x += 0.5f)
+            {
+                glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f };
+                Vortex::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+            }
+        }
         Vortex::Renderer2D::EndScene();
     }
 }
@@ -60,6 +73,14 @@ void Sandbox2D::OnImGuiRender()
     VT_PROFILE_FUNCTION();
 
     ImGui::Begin("Settings");
+
+    auto stats = Vortex::Renderer2D::GetStats();
+    ImGui::Text("Renderer2D Stats:");
+    ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+    ImGui::Text("Quads: %d", stats.QuadCount);
+    ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+    ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+
     ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
     ImGui::End();
