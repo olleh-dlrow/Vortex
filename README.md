4bc5f794ce1dc4bac9d5da40c210ce7bdc23d4fd
1136246408@qq.com <1136246408@qq.com>
2022-04-29 22:10:30 +0800

    updated vendors

--- README.md
+++ README.md
@@ -4,9 +4,51 @@
 
 a 2D game engine program
 
-support:
+## Support
 
 windows10 x64
+
+
+
+## Third Party
+
+[spdlog](https://github.com/gabime/spdlog.git)
+
+[glfw](https://github.com/glfw/glfw.git)
+
+[imgui](https://github.com/ocornut/imgui.git)
+
+[glm](https://github.com/g-truc/glm.git)
+
+
+
+## Usage
+
+git clone \<repo\> --recursive
+
+
+
+**delete submodule:**
+
+cd Vortex/vendor/moduleA
+
+git rm moduleA
+
+vim .git/config, delete [submodule moduleA]...
+
+rm .git/modules/Vortex/vendor/moduleA
+
+
+
+**add submodule:**
+
+git submodule add url
+
+
+
+premake:
+
+find compiled files in cmake
 
 
 
--- Vortex/src/Vortex/ImGui/ImGuiLayer.cpp
+++ Vortex/src/Vortex/ImGui/ImGuiLayer.cpp
@@ -32,17 +32,14 @@ namespace Vortex {
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
         //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-        //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons;
-        //io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
 
         // Setup Dear ImGui style
         ImGui::StyleColorsDark();
 
         // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
         ImGuiStyle& style = ImGui::GetStyle();
-        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        //if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        if(io.ConfigFlags)
         {
             style.WindowRounding = 0.0f;
             style.Colors[ImGuiCol_WindowBg].w = 1.0f;
@@ -88,11 +85,10 @@ namespace Vortex {
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        if(io.ConfigFlags)
         {
             GLFWwindow* backup_current_context = glfwGetCurrentContext();
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
+            
             glfwMakeContextCurrent(backup_current_context);
         }
     }
